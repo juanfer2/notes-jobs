32a1c28581047ab2a0bb8ba2d0ccf6351318c8b6
juanfer2 <64051193+juanfer2@users.noreply.github.com>
2021-12-07 19:47:24 -0500

    Update package.json

--- src/modules/queries/services/get_queries.ts
+++ src/modules/queries/services/get_queries.ts
@@ -1,12 +1,12 @@
 import { Service } from "typedi";
-import { query } from "../../../clients/prisma_client";
+import { queryScript } from "../../../clients/prisma_client";
 
 @Service()
 class GetProjects {
   constructor(){}
 
   async byUser(user: any, projectId: any) {
-    return await query.findMany({
+    return await queryScript.findMany({
       where: {
         projectId: projectId
       },
